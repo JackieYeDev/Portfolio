ae25bd2b704b5c2bc7ccc56bc44ceec690b2816c
Jackie <jye0325@gmail.com>
2022-07-18 03:12:40 -0400

    Remove useFetchPortfolio hook

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,19 +1,34 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Card, CardGroup, Divider } from "semantic-ui-react";
 import { UserContext } from "../context/user";
-import useFetchPortfolio from "../hooks/useFetchPortfolio";
 import Chart from "./Chart";
+import API_KEY from "../assets/API.json";
 
 function Portfolio() {
   const [user] = useContext(UserContext);
-  const stocksArray = useFetchPortfolio(user.stocks);
+  const [stocksArray, setStocksArray] = useState([]);
 
-  // console.log(stocksArray);
-  if (stocksArray !== null) {
-    stocksArray.forEach(stock =>
-      console.log(stock[Object.keys(stock)]["Time Series (5min)"])
-    );
-  }
+  useEffect(() => {
+    let stocks = [];
+    try {
+      Promise.all(
+        user.stocks.map((stock) =>
+          fetch(
+            `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=5min&apikey=${API_KEY}&symbol=` +
+              stock
+          )
+            .then((res) => res.json())
+            .then((data) => data)
+        )
+      )
+        .then((res) => stocks.push(res))
+        .then(() => {
+          setStocksArray([...stocks[0]]);
+        });
+    } catch (e) {
+      console.error(e);
+    }
+  }, [user.stocks]);
 
   return (
     <div>
@@ -21,14 +36,26 @@ function Portfolio() {
       <h3>Portfolio Page</h3>
       <Divider horizontal></Divider>
 
-      {stocksArray !== null ? (
+      {stocksArray !== [] ? (
         <CardGroup centered itemsPerRow={2}>
           {stocksArray.map((stock, index) => {
-            console.log(stock[Object.keys(stock)]["Time Series (5min)"]);
             return (
               <Card key={index}>
-                <Card.Content>{Object.keys(stock)}</Card.Content>
-                <Card.Content></Card.Content>
+                <Card.Content>{stock["Meta Data"]["2. Symbol"]}</Card.Content>
+                <Card.Content>
+                  <Chart
+                    stockName={stock["Meta Data"]["2. Symbol"]}
+                    labels={Object.keys(stock["Time Series (5min)"])
+                      .map((key) => key.split(" ")[1])
+                      .reverse()}
+                    data={Object.keys(stock["Time Series (5min)"])
+                      .map(
+                        (key) =>
+                          stock["Time Series (5min)"][`${key}`]["4. close"]
+                      )
+                      .reverse()}
+                  />
+                </Card.Content>
               </Card>
             );
           })}
